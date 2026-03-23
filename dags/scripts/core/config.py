8521d9ad628f4defeaebb6ba77f316fbd8a9844c
Toasted-ctrl <themickbb@gmail.com>
2026-03-23 20:48:48 +0100

    Test.

--- dags/scripts/core/config.py
+++ dags/scripts/core/config.py
@@ -6,18 +6,20 @@ class Config:
 
     load_dotenv()
 
-    ingest_db_protocol: str = os.getenv("INGEST_DB_PROTOCOL")
-    ingest_db_connection: str = os.getenv("INGEST_DB_CONNECTION")
+    dia_db_protocol: str = os.getenv("DIA_DB_PROTOCOL")
+    dia_db_connection: str = os.getenv("DIA_DB_CONNECTION")
     
-    ingest_db_hostname: str = os.getenv("INGEST_DB_HOSTNAME")
-    ingest_db_password: str = os.getenv("INGEST_DB_PASSWORD")
-    ingest_db_username: str = os.getenv("INGEST_DB_USERNAME")
-    ingest_db_database: str = os.getenv("INGEST_DB_DATABASE")
-    ingest_db_port: str = os.getenv("INGEST_DB_PORT")
+    dia_db_hostname: str = os.getenv("DIA_DB_HOSTNAME")
+    dia_db_password: str = os.getenv("DIA_DB_PASSWORD")
+    dia_db_username: str = os.getenv("DIA_DB_USERNAME")
+    dia_db_database: str = os.getenv("DIA_DB_DATABASE")
+    dia_db_port: str = os.getenv("DIA_DB_PORT")
+
+    dia_api_key: str = os.getenv("DIA_API_KEY")
 
     @property
-    def ingest_db_url(self):
-        return f"{self.ingest_db_protocol}+{self.ingest_db_connection}://{self.ingest_db_username}:{self.ingest_db_password}@{self.ingest_db_hostname}:{self.ingest_db_port}/{self.ingest_db_database}"
+    def dia_db_url(self):
+        return f"{self.dia_db_protocol}+{self.dia_db_connection}://{self.dia_db_username}:{self.dia_db_password}@{self.dia_db_hostname}:{self.dia_db_port}/{self.dia_db_database}"
     
     print("> Config initialized")
 
--- dags/scripts/requests.py
+++ dags/scripts/requests.py
@@ -4,4 +4,5 @@ from scripts.core.config import config
 
 @task
 def request_json():
-    print(config.ingest_db_url)
+    print(f"DB url: {config.dia_db_url}")
+    print(f"Key: {config.dia_api_key}")
