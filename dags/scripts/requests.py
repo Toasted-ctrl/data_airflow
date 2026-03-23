2e54e3c64fb6acd1179842ebfb4a620b2e2c178a
Toasted-ctrl <themickbb@gmail.com>
2026-03-23 19:53:05 +0100

    Test.

--- dags/scripts/requests.py
+++ dags/scripts/requests.py
@@ -6,3 +6,6 @@ from dotenv import load_dotenv
 @task
 def request_json():
     print("Hello from this function")
+    load_dotenv()
+    hostname = os.getenv("INGEST_DB_HOSTNAME")
+    print(hostname)
