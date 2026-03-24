b84313a4450e94d6f85d57d3491a669690a689be
Toasted-ctrl <themickbb@gmail.com>
2026-03-24 19:03:52 +0100

    Update.

--- dags/scripts/custom/DIA/dia_fetch_sources.py
+++ dags/scripts/custom/DIA/dia_fetch_sources.py
@@ -3,8 +3,10 @@ from sqlalchemy.orm import Session
 from scripts.custom.DIA.dia_schema import Sources
 
 def fetch_sources(db: Session, sequence: str, type: str) -> list:
+    # NOTE: Distinct can't be used as  postgres cannot compare JSON entries.
+    # NOTE: We'll just need to make sure that every entry in the sources table is unique.
     query = db.query(
-        Sources).filter(Sources.sequence == sequence, Sources.type == type).distinct().all()
+        Sources).filter(Sources.sequence == sequence, Sources.type == type).all()
     if len(query) == 0:
         return ['test_1', 'test_2']
     return query
