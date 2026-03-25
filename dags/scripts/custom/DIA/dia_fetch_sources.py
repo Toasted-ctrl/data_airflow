2437888950fda0f3311d97c967d58b8e61764795
Toasted-ctrl <themickbb@gmail.com>
2026-03-25 18:53:33 +0100

    Updated so we return a list of dictionaries, and not a list of sqlalchemy source objects.

--- dags/scripts/custom/DIA/dia_fetch_sources.py
+++ dags/scripts/custom/DIA/dia_fetch_sources.py
@@ -3,10 +3,23 @@ from sqlalchemy.orm import Session
 from scripts.custom.DIA.dia_schema import Sources
 
 def fetch_sources(db: Session, sequence: str, type: str) -> list:
+    
     # NOTE: Distinct can't be used as  postgres cannot compare JSON entries.
     # NOTE: We'll just need to make sure that every entry in the sources table is unique.
-    query = db.query(
-        Sources).filter(Sources.sequence == sequence, Sources.type == type).all()
-    if len(query) == 0:
+
+    # NOTE: We'll also need to create a dictionary as we cannot return a list of sqlalchemy objects
+    # NOTE: in airflow.
+
+    query = db.query(Sources).filter(Sources.sequence == sequence, Sources.type == type).all()
+    if not query:
         return ['test_1', 'test_2']
-    return query
+    return [
+        {
+            "item_id": row.item_id,
+            "base_url": row.base_url,
+            "url_ext": row.url_ext,
+            "params": row.params,
+            "credentials": row.credentials
+        }
+        for row in query
+    ]
