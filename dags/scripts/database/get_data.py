43e7c552aa5f4a2c1b731ffb7beb4e68557a35c7
Toasted-ctrl <themickbb@gmail.com>
2026-04-04 13:45:00 +0200

    Updated docs.

--- dags/scripts/database/get_data.py
+++ dags/scripts/database/get_data.py
@@ -5,7 +5,7 @@ def get_all_filter_by(table_schema, db: Session, filter_by_values: dict, return_
     
     """Retrieves a list of rows based on specified filters. The filters need to be defined as a dict.
     The return fields are the columns you'd like to receive from the database query, and must be defined 
-    in a list. Will raise an error if no data is found."""
+    in a list. Will raise EmptyDataError if no data is found."""
 
     query = db.query(table_schema).filter_by(**filter_by_values).all()
     if not query:
