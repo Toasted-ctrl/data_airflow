fae0c82e1ab1c4df3377abe14b2bcabe4b21d146
Toasted-ctrl <themickbb@gmail.com>
2026-04-04 13:19:38 +0200

    Testing new dynamic method to fetch data from db.

--- dags/scripts/configs/DIA.py
+++ dags/scripts/configs/DIA.py
@@ -1,5 +1,10 @@
 import os
+
 from dotenv import load_dotenv
+from sqlalchemy import Column, Integer, String, Boolean, JSON, DateTime, func
+from sqlalchemy.orm import DeclarativeBase
+
+# DIA CONFIGURATION
 
 class DiaConfig:
     print("Initializing Config: DIA")
@@ -26,3 +31,22 @@ class DiaConfig:
     print("Initialized Config: DIA")
 
 dia_config = DiaConfig()
+
+# DIA SOURCES SCHEMA
+
+class Base(DeclarativeBase):
+    pass
+
+class Sources(Base):
+    __tablename__ = 'sources'
+
+    source_id = Column(Integer, primary_key=True)
+    description = Column(String(200), nullable=False)
+    base_url = Column(String(100), nullable=False)
+    url_ext = Column(String(100), nullable=True)
+    params = Column(JSON, nullable=True)
+    content_type = Column(String(40), nullable=False)
+    sequence = Column(String(20), nullable=False)
+    headers = Column(JSON, nullable=True)
+    inserted = Column(DateTime(timezone=False), server_default=func.now(), nullable=False)
+    is_active = Column(Boolean, nullable=False)
---
+++ dags/scripts/database/get_data.py
@@ -0,0 +1,13 @@
+from database.session import get_db
+from sqlalchemy.orm import Session
+
+def get_all(table_schema, db: Session, filter_by_values: dict, return_fields: list) -> list:
+    
+    """Retrieves list of items for a given set of filters. The required return values must be specified."""
+
+    query = db.query(table_schema).filter_by(**filter_by_values).all()
+    if not query:
+        raise ValueError("No data.")
+    return [
+        {field: getattr(query_row, field) for field in return_fields} for query_row in query
+    ]
