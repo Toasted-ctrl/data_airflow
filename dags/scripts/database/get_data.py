7bb5683b3e25dab39aa1421b5930f12b6f67f058
Toasted-ctrl <themickbb@gmail.com>
2026-04-04 13:43:04 +0200

    Added EmptyDataError from Pandas in case the query returns no data.

--- dags/scripts/database/get_data.py
+++ dags/scripts/database/get_data.py
@@ -1,12 +1,15 @@
+from pandas.errors import EmptyDataError
 from sqlalchemy.orm import Session
 
 def get_all_filter_by(table_schema, db: Session, filter_by_values: dict, return_fields: list) -> list:
     
-    """Retrieves list of items for a given set of filters. The required return values must be specified."""
+    """Retrieves a list of rows based on specified filters. The filters need to be defined as a dict.
+    The return fields are the columns you'd like to receive from the database query, and must be defined 
+    in a list. Will raise an error if no data is found."""
 
     query = db.query(table_schema).filter_by(**filter_by_values).all()
     if not query:
-        raise ValueError("No data.")
+        raise EmptyDataError
     return [
         {field: getattr(query_row, field) for field in return_fields} for query_row in query
     ]
