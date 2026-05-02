6037749dfb0b1a86cf4e73680e7780f96307d696
Toasted-ctrl <themickbb@gmail.com>
2026-05-02 17:24:14 +0200

    Implemented table_exists and create_table functions.

--- dags/scripts/database/table.py
+++ dags/scripts/database/table.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import DeclarativeBase
 from typing import Type
 
+class TableCreationError(RuntimeError):
+    """Raised when a database table could not be created."""
+
 def table_exists(table: Type[DeclarativeBase], db_url: str) -> bool:
 
     """Checks if the table exists in the specified database."""
@@ -12,13 +15,14 @@ def table_exists(table: Type[DeclarativeBase], db_url: str) -> bool:
     finally:
         engine.dispose()
 
-def create_table(table: Type[DeclarativeBase], db_url) -> bool:
+def create_table(table_schema: Type[DeclarativeBase], db_url: str) -> None:
 
     """Returns False is table creation failed"""
 
     engine = create_engine(url=db_url)
     try:
-        table.__table__.create(bind=engine, checkfirst=True)
-        return inspect(engine).has_table(table_name=table.__tablename__)
+        table_schema.__table__.create(bind=engine, checkfirst=True)
+        if not inspect(engine).has_table(table_name=table_schema.__tablename__):
+            raise TableCreationError(f"Could not create {table_schema.__tablename__}")
     finally:
         engine.dispose()
