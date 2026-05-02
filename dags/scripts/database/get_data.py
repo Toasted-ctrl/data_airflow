d1d331ee1f3b936965112aec1ec62fd40272cad7
Toasted-ctrl <themickbb@gmail.com>
2026-05-02 17:24:50 +0200

    Implemented get_max_filter_by function.

--- dags/scripts/database/get_data.py
+++ dags/scripts/database/get_data.py
@@ -1,7 +1,13 @@
 from pandas.errors import EmptyDataError
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 
-def get_all_filter_by(table_schema, db: Session, filter_by_values: dict, return_fields: list) -> list:
+def get_all_filter_by(
+        table_schema,
+        db: Session,
+        filter_by_values: dict,
+        return_fields: list
+) -> list:
     
     """Retrieves a list of rows based on specified filters. The filters need to be defined as a dict.
     The return fields are the columns you'd like to receive from the database query, and must be defined 
@@ -13,3 +19,23 @@ def get_all_filter_by(table_schema, db: Session, filter_by_values: dict, return_
     return [
         {field: getattr(query_row, field) for field in return_fields} for query_row in query
     ]
+
+def get_max_filter_by(
+        table_schema, db: Session,
+        filter_by_values: dict,
+        return_field: int | float
+) -> int:
+
+    """Retrieves the maximum value within a specified column based on a set of filters.
+    The column of which the max needs to be determined must be of type integer or float."""
+
+    column = getattr(table_schema, return_field)
+
+    max_value = (
+        db.query(func.max(column))
+        .filter_by(**filter_by_values)
+        .scalar()
+    )
+    if not max_value:
+        raise EmptyDataError
+    return max_value
