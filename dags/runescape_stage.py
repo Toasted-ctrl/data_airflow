f2c68158b2d61444379e440953e6939a6665d8a1
Toasted-ctrl <themickbb@gmail.com>
2026-05-03 13:42:14 +0200

    Implemented get_filter function.

--- dags/runescape_stage.py
+++ dags/runescape_stage.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from sqlalchemy.orm import DeclarativeBase
 from typing import Type
 
-from scripts.database.get_data import get_all_filter_by, get_max_filter_by
+from scripts.database.get_data import get_all_filter_by, get_max_filter_by, get_all_filters
 from scripts.database.session import get_db
 from scripts.database.table import table_exists, create_table
 
@@ -41,8 +41,8 @@ def get_max_ingested_record(
     min_record_name: str
 ) -> int:
     
-    """We're using this to get which record we ingested into stage-1 last.
-    This way we only have to focus on the new entried in ingest > incremental load."""
+    # NOTE: We're using this to get which record we ingested into stage-1 last.
+    # NOTE: This way we only have to focus on the new entried in ingest > incremental load.
 
     is_table_present: bool = table_exists(table_schema=table_schema, db_url=runescape_config.db_url)
     if not is_table_present:
@@ -52,9 +52,23 @@ def get_max_ingested_record(
     return get_max_filter_by(
         table_schema=table_schema,
         db=next(db),
-        filter_by_values={},    # Leave this empty, just getting the max is enough.
+        filter_by_values={},    # NOTE: Leave this empty, just getting the max is enough.
         return_field=min_record_name
     )
+
+@task.python
+def get_filter(
+    max_record: int,
+    source_ids: list[int]
+) -> dict:
+    
+    """Creates filter to use with get_all_filters."""
+
+    return {
+        "ingested_item_id__gt": max_record,
+        "source_id": source_ids
+    }
+    
 
 with DAG(
     dag_id="runescape.stage.1.hiscores.incremental",
@@ -66,7 +80,18 @@ with DAG(
     # NOTE: Determine the max ingest_item_id we've already loaded into stage-1.
     # NOTE: If the table does not exist, create table and 0.
 
-    _source_ids = get_source_ids(description=runescape_config.description_hiscores)
-    _max_ingested_item_id = get_max_ingested_record(table_schema=StageHiscores_1, min_record_name="ingest_item_id")
+    _source_ids = get_source_ids(
+        description=runescape_config.description_hiscores
+    )
+
+    _max_ingested_item_id = get_max_ingested_record(
+        table_schema=StageHiscores_1,
+        min_record_name="ingest_item_id"
+    )
+
+    _filter = create_filter(
+        max_record=_max_ingested_item_id,
+        source_ids=_source_ids
+    )
 
     # TODO: Only test to check if we're getting the correct values and to see if the table gets created.
